7acc675ef4e8713e2ce7a354e80281a9ae4f8eed
JuanDAM96 <juan23655@gmail.com>
2025-03-25 13:16:46 +0100

    commit JALM

--- App.java
+++ App.java
@@ -1,11 +1,15 @@
 import java.io.File;
 import java.io.IOException;
-
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Clase App
  * 
- * La Clase Principal donde se llama y declara las clases y sus metodos, además de ejecutarse el codigo
+ * La Clase Principal donde se llama y declara las clases y sus metodos, además
+ * de ejecutarse el codigo
+ * 
  * @author Santiago
  * @author Juan
  * @version 0.1
@@ -18,22 +22,43 @@ public class App {
         File archivoConfiguracion = new File(nombreArchivoConfiguracion);
 
         if (archivoConfiguracion.exists()) {
-            System.out.println("El archivo de configuración '" + nombreArchivoConfiguracion + "' ya existe. El programa termina.");
+            System.out.println(
+                    "El archivo de configuración '" + nombreArchivoConfiguracion + "' ya existe. El programa termina.");
             return;
         } else {
             try {
                 if (archivoConfiguracion.createNewFile()) {
-                    System.out.println("El archivo de configuración '" + nombreArchivoConfiguracion + "' ha sido creado.");
+                    System.out.println(
+                            "El archivo de configuración '" + nombreArchivoConfiguracion + "' ha sido creado.");
                 } else {
-                    System.err.println("No se pudo crear el archivo de configuración '" + nombreArchivoConfiguracion + "'.");
+                    System.err.println(
+                            "No se pudo crear el archivo de configuración '" + nombreArchivoConfiguracion + "'.");
                     return;
                 }
             } catch (IOException e) {
                 System.err.println("Error al crear el archivo de configuración: " + e.getMessage());
                 e.printStackTrace();
-                return; 
+                return;
             }
         }
+
+        String[] directorios = { "escenarios", "jugadores", "partidas" };
+
+        for (String nombreDirectorios : directorios) {
+            Path rutaDirectorio = Paths.get(nombreDirectorios);
+            if (!Files.exists(rutaDirectorio) || !Files.isDirectory(rutaDirectorio)) {
+                try {
+                    Files.createDirectories(rutaDirectorio);
+                    System.out.println("Directorio '" + nombreDirectorios + "' creado.");
+                } catch (IOException e) {
+                    System.err.println("No se pudo crear el directorio '" + nombreDirectorios + "': " + e.getMessage());
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("El directorio '" + nombreDirectorios + "' ya existe.");
+            }
+        }
+        System.out.println("El programa ha finalizado su ejecución.");
 
     }
 }
